5609752a50b01a1e99baca67c520fc40a4b4788f
Leonel Velasquez <leonelvelasquez@gmail.com>
2020-02-12 10:52:34 -0600

    add mysql validation

--- resources/templates/front/side_nav.php
+++ resources/templates/front/side_nav.php
@@ -5,6 +5,12 @@
                     <?php 
                         $query = "SELECT * FROM categories";
                         $send_query = mysqli_query($connection, $query);
+
+                        if(!$send_query){
+                            
+                            die("QUERY FAILED", mysqli_error($connection));
+                        }
+
                         while($row = mysqli_fetch_array($send_query)) {
                             echo "<a href='#' class='list-group-item'>{$row[1]}</a>";
                         }
